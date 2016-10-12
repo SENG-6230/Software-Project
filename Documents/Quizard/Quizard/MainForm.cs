99a288ea80d620008820582696655d2b1d4fa8b8
master9559 <master9559@yahoo.com>
2016-10-11 23:29:31 -0400

    adding a quizard SLN to our repo so we can begin the development work

---
+++ Documents/Quizard/Quizard/MainForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quizard
+{
+    public partial class MainForm : Form
+    {
+        public bool Loaded
+        {
+            get
+            {
+                return loaded;
+            }
+        }
+        bool loaded = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            loaded = false;
+        }
+    }
+}
---
+++ Documents/Quizard/Quizard/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quizard
+{
+    static class Program
+    {
+        static MainForm mainForm;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            SingleInstanceController singleInstanceController = new SingleInstanceController();
+            singleInstanceController.Run(args);
+        }
+
+        public class SingleInstanceController : WindowsFormsApplicationBase
+        {
+            string[] args = null;
+
+            public SingleInstanceController()
+            {
+                IsSingleInstance = true;
+            }
+
+            protected override bool OnStartup(StartupEventArgs e)
+            {
+                bool returnValue = base.OnStartup(e);
+                args = e.CommandLine.ToArray();
+
+                return returnValue;
+            }
+
+            protected override void OnStartupNextInstance(StartupNextInstanceEventArgs e)
+            {
+                base.OnStartupNextInstance(e);
+                try
+                {
+                    if (!mainForm.Loaded)
+                    {
+
+                    }
+                    else
+                    {
+                        args = e.CommandLine.ToArray();
+                        processCommandLine();
+                    }
+                }
+                catch (Exception exc)
+                {
+                }
+            }
+
+            protected override void OnCreateMainForm()
+            {
+                mainForm = new MainForm();
+                MainForm = mainForm;
+                try
+                {
+                    processCommandLine();
+                }
+                catch (Exception e)
+                {
+                    Environment.Exit(1);
+                }
+            }
+
+            private void processCommandLine()
+            {
+            }
+        }
+    }
+}
