bb8062650ae42b4c99f9723981074b09b48c10d2
master9559 <master9559@yahoo.com>
2016-10-26 18:08:21 -0400

    adding in a database command

--- Documents/Quizard/Quizard/Database/QuizardDatabase.cs
+++ Documents/Quizard/Quizard/Database/QuizardDatabase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,38 @@ namespace Quizard
     {
         internal void Open()
         {
-
+            try
+            {
+                //add database file path login here
+            }
+            catch { }
         }
 
-        internal void CreateUser(string Name, string email, string password, string role)
+        private void executeCommand(Action<SQLiteCommand> commandAction)
         {
+            using (SQLiteConnection database = new System.Data.SQLite.SQLiteConnection("Data Source =../../quizard.db"))
+            {
+                database.Open();
+                using (SQLiteCommand command = database.CreateCommand())
+                {
+                    commandAction.Invoke(command);
+                }
+            }
+        }
+
+        internal int CreateUser(string Name, string email, string password, string role)
+        {
+            int results = 0;
             //add sql database code here to add a user
+            executeCommand(delegate(SQLiteCommand command) 
+            {
+                command.CommandText =
+                    "INSERT INTO users(rowid, fname, lname, email, user_type, password) "
+                    + "VALUES(NULL, " + Name + ", " + Name + ", " + email + ", " + (UserTypes)Enum.Parse(typeof(UserTypes), role) + ", " + password + ");";
+
+                results = command.ExecuteNonQuery();
+            });
+            return results;
         }
 
         internal void CreateClass(string text, string selectedText1, string selectedText2, List<string> list1, List<string> list2)
